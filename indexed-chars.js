e49de17055b617acd58cc5dcfb3b4ec8805262ed
cc54321 <112970207+cc54321@users.noreply.github.com>
2022-10-08 12:33:04 -0400

    Update indexed-chars.js

--- indexed-chars.js
+++ indexed-chars.js
@@ -1,1 +1,34 @@
 
+
+/*
+indexedChars - adds the index of each character before that character in the given string
+Examples:
+indexedChars('hello') -> '0h1e2l3l4o'
+indexedChars('bye') -> '0b1y2e'
+Hints:
+We can add something BEFORE the current character as well!
+create a function> done
+create index >done
+create for loop done 
+create new string
+return
+consolelog twice
+
+
+*/
+
+function indexedChars(str1){
+
+    let newsStr = "";
+
+     for (let i = 0; i < str1.length; i++){
+
+        newsStr += i;
+        newsStr += str1[i];
+
+     }
+     return newsStr;
+}
+
+console.log(indexedChars("Hello"));
+console.log(indexedChars("Bye"));
