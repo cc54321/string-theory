559e9b8a7c08faa44fc22379036fe2500d7b4cbc
cc54321 <112970207+cc54321@users.noreply.github.com>
2022-10-08 12:55:31 -0400

    Update only-vowels.js

--- only-vowels.js
+++ only-vowels.js
@@ -1,1 +1,36 @@
 
+
+/*
+onlyVowels - returns only the vowels from a word
+Examples:
+onlyVowels('Colin Jaffe') -> 'oiae'
+onlyVowels('quickly') -> 'ui'
+onlyVowels('Anthony DeRosa') -> 'Aoeoa'
+CREATE FUNCTION
+ONLY VOWS  AEIOU to show
+create new string
+for loop 
+if state
+return
+console log three times as per above
+*/
+
+
+
+
+function onlyVows(str1){
+   let newstr = "";
+   
+   for (let i = 0; i < str1.length; i++){
+
+    if(str1[i] === "a" || str1[i] === "e" || str1[i] === "i" || str1[i] === "o" || str1[i] === "u"){
+
+        newstr += str1[i]
+    }
+}
+return newstr;
+}
+console.log(onlyVows('Colin Jaffe'))
+console.log(onlyVows('quickly'))
+console.log(onlyVows('Anthony DeRosa'))
+
