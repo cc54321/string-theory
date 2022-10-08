31f58a7b16df8cd4c1d9ecccb17eb7f81c78b325
cc54321 <112970207+cc54321@users.noreply.github.com>
2022-10-08 12:40:29 -0400

    Update ci-emailify.js

--- ci-emailify.js
+++ ci-emailify.js
@@ -1,1 +1,33 @@
 
+
+/*
+ciEmailify - creates an email from a two-part name
+Examples:
+ciEmailify('colin jaffe') -> 'colin.jaffe@codeimmersives.com'
+ciEmailify('Anthony DeRosa') -> 'anthony.derosa@codeimmersives.com'
+create a functiom
+create 2 students email accounts
+from a two part name ('colin jaffe') ('Anthony DeRosa')
+colin.jaffe@codeimmersives.com'
+*/
+
+
+function ciEmailify(str1){
+
+    let newstr = "";
+
+    for(let i = 0; i < str1.length; i++){
+        if(str1[i] === " "){
+            newstr += ".";
+
+        } else {
+            newstr += str1[i];
+        }
+    }
+
+    return newstr.toLowerCase() + "@codeimmersives.com";
+}
+
+    
+    console.log(ciEmailify('colin jaffe'));
+    console.log(ciEmailify('Anthony Derosa') );
