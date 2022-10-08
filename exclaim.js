0011590ee596dcc10bd2f79fd0705e272277d147
cc54321 <112970207+cc54321@users.noreply.github.com>
2022-10-08 12:36:45 -0400

    Update exclaim.js

--- exclaim.js
+++ exclaim.js
@@ -1,1 +1,32 @@
 
+
+/*
+exclaim - returns the given sentence with every question mark or period changed to an exclamation point
+Examples:
+exclaim('What are you doing? Are you a fool?') -> 'What are you doing! Are you a fool!'
+exclaim('This is fine.') -> 'This is fine!'
+
+every question mark and period will change to !
+create a function
+
+*/
+
+function exclaim(str1){
+
+let newstr = "";
+
+for(let i = 0; i < str1.length; i++){
+
+    if(str1[i] === "?" || str1[i] === "."){
+        
+        newstr += "!";
+    }else {
+        newstr += str1[i];
+    }
+}
+return newstr;
+}
+
+console.log(exclaim('What are you doing? Are you a fool?'));
+console.log(exclaim('This is fine.'));
+
