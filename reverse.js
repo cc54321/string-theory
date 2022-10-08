eb378e0552e8b8a2a48ca5fcec6a7da520dc6d01
cc54321 <112970207+cc54321@users.noreply.github.com>
2022-10-08 12:47:51 -0400

    Update reverse.js

--- reverse.js
+++ reverse.js
@@ -1,1 +1,23 @@
 
+/*
+reverse - reverses the given string
+Examples:
+reverse reverse('colin') -> 'niloc'
+reverse('mesuara') -> 'arausem'
+
+create function
+reverse name reverse('colin') -> 'niloc'
+reverse name ('mesuara') -> 'arausem
+*/
+
+function reverse(str1){
+
+    let backstring = "";
+
+    for(let i = str1.length -1; i >= 0; i--){
+        backstring += str1[i];
+    }
+        return backstring;
+}
+console.log(reverse("mesuara"));
+console.log(reverse("colin"));
